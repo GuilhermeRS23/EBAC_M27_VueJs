d887944f8001e70f1d30b71c6ba55615eb84d19a
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2025-05-20 23:25:06 -0300

    Componetizando a aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup>
+import Header from "./components/Header.vue";
+import Form from "./components/Form.vue";
+import ListTask from "./components/ListTask.vue";
 import { reactive } from "vue";
 
 const estado = reactive({
@@ -50,43 +53,10 @@ const adicionarTarefa = () => {
 
 <template>
   <div class="container">
-    <header class="p-5 mb-4 bg-light rounded-3">
-      <h1>Lista de Tarefas</h1>
-      <p>Você possui {{ getTarefasPendentes().length }} tarefas pendentes.</p>
-    </header>
-    <form @submit.prevent="adicionarTarefa">
-      <div class="row">
-        <div class="col">
-          <input class="form-control" type="text" placeholder="Digite uma tarefa..." :value="estado.tarefaTemp"
-            @change="e => estado.tarefaTemp = e.target.value" required>
-        </div>
-        <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Adicionar</button>
-        </div>
-        <div class="col-md-2">
-          <select class="form-control" @change="e => estado.filtro = e.target.value">
-            <option value="todas">Todas as tarefas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="finalizadas">Finalizadas</option>
-          </select>
-        </div>
-      </div>
-
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefa in getFiltroTarefas()">
-        <input @change="e => tarefa.finalizada = e.target.checked" :checked="tarefa.finalizada" :id="tarefa.descricao"
-          type="checkbox">
-        <label :for="tarefa.descricao" class="ms-3" :class="{ done: tarefa.finalizada }">
-          {{ tarefa.descricao }}
-        </label>
-      </li>
-    </ul>
+    <Header :tarefas-pendentes="getTarefasPendentes().length" />
+    <Form :adicionar-tarefa="adicionarTarefa" :tarefa-temp="estado.tarefaTemp"
+      :alterar-temp="e => estado.tarefaTemp = e.target.value" :alterar-tarefa="e => estado.tarefaTemp = e.target.value"
+      :filtro="e => estado.filtro = e.target.value" />
+    <ListTask :tarefas="getFiltroTarefas()" />
   </div>
 </template>
-
-<style scoped>
-.done {
-  text-decoration: line-through;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps(["tarefasPendentes"]);
+</script>
+<template>
+    <header class="p-5 mb-4 bg-light rounded-3">
+        <h1>Lista de Tarefas</h1>
+        <p>Você possui {{ props.tarefasPendentes }} tarefas pendentes.</p>
+    </header>
+</template>
