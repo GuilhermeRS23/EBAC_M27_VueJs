a0c1c8dd30d7bff0c1a539da8fe2e896d3835223
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2025-05-21 21:20:19 -0300

    Finalizando Tarefa

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,45 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+import CampoValores from './components/CampoValores.vue';
+import Header from './components/Header.vue';
+
+const state = reactive({
+  operador: "+",
+  numeroA: 0,
+  numeroB: 0,
+});
+
+const resultado = () => {
+  const { operador } = state;
+  const num1 = Number(state.numeroA);
+  const num2 = Number(state.numeroB);
+
+  if (operador === "-") {
+    return num1 - num2;
+  } else if (operador === "/") {
+    return (num1 / num2).toFixed(2);
+  } else if (operador === "*") {
+    return num1 * num2;
+  } else {
+    return num1 + num2;
+  }
+};
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="calculadora">
+    <Header />
+    <CampoValores :numero-a="state.numeroA" :alterar-valor-a="e => state.numeroA = e.target.value"
+      :operador="e => state.operador = e.target.value" :numero-b="state.numeroB"
+      :alterar-valor-b="e => state.numeroB = e.target.value" :resultado="resultado()" />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.calculadora {
+  align-items: center;
+  margin: auto;
+  max-width: 720px;
 }
 </style>
---
+++ src/components/CampoValores.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps(["resultado", "operador", "numeroA", "numeroB", "alterarValorA", "alterarValorB"]);
+</script>
+
+<template>
+    <div class="row g-3 p-5 mb-4 bg-light rounded-3 text-center">
+        <div class="col-3">
+            <input type="number" class="form-control text-center" :value="props.numeroA" @keyup="props.alterarValorA">
+        </div>
+        <div class="col-2">
+            <select class="form-control text-center" @change="props.operador">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="/">:</option>
+                <option value="*">x</option>
+            </select>
+        </div>
+        <div class="col-3">
+            <input type="number" class="form-control text-center" :value="props.numeroB" @keyup="props.alterarValorB">
+        </div>
+        <div class="col-1">
+            <input readonly class="text-center form-control" value="=">
+        </div>
+        <div class="col-3">
+            <input readonly class="form-control text-center p-2" :value="props.resultado">
+        </div>
+    </div>
+</template>
